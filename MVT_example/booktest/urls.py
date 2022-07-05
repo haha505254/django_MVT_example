19b0d930bd6be38fd5680f81afd2be5a68fa7487
haha505254 <haha505254@gmail.com>
2022-07-05 20:23:38 +0800

    version 1.0

--- MVT_example/booktest/urls.py
+++ MVT_example/booktest/urls.py
@@ -3,4 +3,6 @@ from django.urls import path
 from booktest import views
 urlpatterns = [
     path('index/', views.index),
+    path('books/', views.show_books),
+    path('books/<int:bid>', views.detail),
 ]
--- MVT_example/booktest/views.py
+++ MVT_example/booktest/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from booktest.models import BookInfo
 # Create your views here.
 def index(request):
-    return HttpResponse('okk')
+    return render(request,'booktest/index.html')
+
+def show_books(request):
+    #通過Model查詢圖書數據
+    books = BookInfo.objects.all()
+    return render(request,'booktest/show_books.html',locals())
+
+def detail(request,bid):
+    
+    books = BookInfo.objects.get(id=bid)
+    heros = books.heroinfo_set.all()
+    return render(request,'booktest/detail.html',locals())
